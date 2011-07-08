3fd4239308fc46cd9fea555a98e33dda264d20bb
podviaznikov <podviaznikov@ubuntu.ubuntu-domain>
2011-07-08 21:27:17 +0200

    updates in the API. more utility methods

--- audioEL.js
+++ audioEL.js
@@ -1,36 +1,65 @@
 var AudioEl={
-    newEl(id){
+    this.newEl(id,src)=function{
         _.extend(this,Backbone.Events);
         this.el=$('#'+id);
+        if(url){
+            this.el.setAttribute('src',url);
+        }
         this.el.bind('timeupdate',this.timeupdate);
+        this.el.bind('pause',this.pause);
     },
-    timeupdate:function(){
-       this.trigger('audio:update',this.el.duration,this.el.currentTime);
+    this.timeupdate=function(){
+        var time=this.getTime(),
+            duration=this.getDuration(),
+            remaining=parseInt(duration-time,10);
+        if(remaining===0){
+            this.trigger('finished');
+        }
+        else{
+            this.trigger('updated',duration,time);
+        }
+//    var timeInSeconds=parseInt(currentTime,10),
+//                songDuration=parseInt(duration,10),
+//                rem=parseInt(duration-currentTime,10),
+//                pos=(timeInSeconds/duration) * 100,
+//                mins=Math.floor(currentTime/60,10),
+//                secs=timeInSeconds-mins*60,
+//                timeCounter= mins + ':' + (secs > 9 ? secs : '0' + secs),
+
     },
-    play:function(url){
+    this.play=function(url){
         if(url){
             this.el.setAttribute('src',url);
         }
         this.el.play();
+        this.trigger('started');
     },
-    pause:function(){
+    this.pause=function(){
         this.el.pause();
+        this.trigger('paused');
     },
-    stop:function(){
+    this.stop=function(){
         this.pause();
         this.el.currentTime=0;
+        this.trigger('stopped');
     },
-    setVolume:function(volume){
+    this.setVolume=function(volume){
         this.el.volume=volume;
     },
-    getVolume:function(){
+    this.getVolume=function(){
         return this.el.volume;
     },
-    getDuration:function(){
-        return this.el.duration;
+    this.getDuration=function(){
+        return parseInt(this.el.duration,10);
     },
-    setTime:function(time){
+    this.setTime=function(time){
         this.el.currentTime=time;
-        this.trigger('audio:update',this.el.duration,time);
+        this.trigger('updated',this.el.duration,time);
+    },
+    this.getTime=function(){
+        return parseInt(this.el.currentTime,10);
+    },
+    this.getPercent=function(){
+        return (this.getTime()/this.getDuration())*100;
     }
 };
